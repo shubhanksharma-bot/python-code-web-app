6387e655ef398b69264f1643e777d388a9ca74d4
shubhank <shubhank.sharma@mba.christuniversity.in>
2025-11-15 23:15:38 +0530

    fix app and yml

--- app.py
+++ app.py
@@ -2,8 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def home():
     return "Hello Azure! Flask on Python 3.11."
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
